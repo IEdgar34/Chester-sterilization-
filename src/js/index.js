8de0a705a7238f7d91a457b74d7b5c8f94c03e7d
Edgar <edo.jan.94@bk.ru>
2025-05-04 00:36:12 +0300

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,20 @@
-import Swiper from 'swiper';
-import 'swiper/css';
-import { Navigation, Pagination } from 'swiper/modules';
+import Swiper from "swiper";
+import "swiper/css";
+import { Navigation, Pagination } from "swiper/modules";
 import { menuOfIcons } from "./modules/menioficons";
-import { mainSwiper } from './modules/swiper';
-import { teamSlider } from './modules/teamslider';
-import { photogallerySlider } from './modules/photogallery';
+import { mainSwiper } from "./modules/swiper";
+import { teamSlider } from "./modules/teamslider";
+import { photogallerySlider } from "./modules/photogallery";
 document.addEventListener("DOMContentLoaded", () => {
     menuOfIcons();
-    mainSwiper(Swiper,Navigation,Pagination)
-    teamSlider(Swiper,Navigation,Pagination)
-    photogallerySlider(Swiper,Navigation)
+    mainSwiper(Swiper, Navigation, Pagination);
+    teamSlider(Swiper, Navigation, Pagination);
+    photogallerySlider(Swiper, Navigation);
+
+    function setVh() {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }
+    setVh();
+    window.addEventListener("resize", setVh);
 });
